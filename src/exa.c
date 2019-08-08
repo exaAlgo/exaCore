b250b4dc3b43e1f58413d97e24532cfb6b4079e2
Thilina Rathnayake <thilinarmtb@gmail.com>
2019-08-08 09:24:38 -0500

    Add src/exa.c

---
+++ src/exa.c
@@ -0,0 +1,75 @@
+#include "exa-impl.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+//
+// exaInit
+//
+int exaInit(exaHandle *h, exaCommExternal ce) {
+  exaMalloc(1, h);
+  exaHandle h_ = *h;
+
+  exaCreateComm(&h_->global, ce);
+  exaCreateComm(&h_->local, ce);
+
+  h_->elementArray.ptr = NULL;
+  h_->elementArray.n = (*h)->elementArray.max = 0;
+
+  h_->dbgLevel = 0;
+  h_->printStat = 0;
+
+  exaMalloc(1,&h_->histogram);
+  return 0;
+}
+//
+// exaFinalize
+//
+int exaFinalize(exaHandle h) {
+  if(exaGetGlobalComm(h))
+    exaDestroyComm(exaGetGlobalComm(h));
+  if(exaGetLocalComm(h))
+    exaDestroyComm(h->local);
+
+  array_free(&(h->elementArray));
+
+  exaFree(h->histogram);
+
+  exaFree(h);
+
+  return 0;
+}
+//
+// exaMalloc, Realloc, Calloc and Free
+//
+int exaMallocArray(size_t n, size_t unit, void *p) {
+  int ierr = posix_memalign((void **)p, EXA_ALIGN, n * unit);
+  if(ierr)
+    printf("exaMallocArray Failed: %s:%d\n", __FILE__, __LINE__);
+  return ierr;
+}
+
+int exaCallocArray(size_t n, size_t unit, void *p) {
+  int ierr = 0;
+  *(void **)p = calloc(n, unit);
+  if(n && unit && !*(void **)p) {
+    ierr = 1;
+    printf("exaCallocArray Failed: %s:%d\n", __FILE__, __LINE__);
+  }
+  return ierr;
+}
+
+int exaReallocArray(size_t n, size_t unit, void *p) {
+  int ierr = 0;
+  *(void **)p = realloc(*(void **)p, n * unit);
+  if(n && unit && !*(void **)p) {
+    ierr = 1;
+    printf("exaReallocArray Failed: %s:%d\n", __FILE__, __LINE__);
+  }
+  return ierr;
+}
+
+int exaFree(void *p) {
+  free(p);
+  p = NULL;
+  return 0;
+}
