21e2e5fbef51b5a1384f3a03cc93e0cd306f9169
Thilina Rathnayake <thilinarmtb@gmail.com>
2019-08-20 16:15:12 -0500

    exa Types: Add limits

--- src/exa-types.h
+++ src/exa-types.h
@@ -9,6 +9,10 @@ typedef long long exaLong;
 typedef unsigned long long exaULong;
 #define exaLongFormat "%lld"
 #define exaULongFormat "%llu"
+#define exaLongMAX LLONG_MAX
+#define exaLongMIN LLONG_MIN
+#define exaULongMAX ULLONG_MAX
+#define exaULongMIN 0
 
 #define EXA_LONG MPI_LONG_LONG
 #define EXA_UNSIGNED_LONG MPI_UNSIGNED_LONG_LONG
@@ -19,6 +23,10 @@ typedef int exaInt;
 typedef unsigned int exaUInt;
 #define exaIntFormat "%d"
 #define exaUIntFormat "%u"
+#define exaIntMAX INT_MAX
+#define exaIntMIN INT_MIN
+#define exaUIntMAX UINT_MAX
+#define exaUIntMIN 0
 
 #define EXA_INT MPI_INT
 #define EXA_UNSIGNED_INT MPI_UNSIGNED_INT
@@ -28,6 +36,8 @@ typedef unsigned int exaUInt;
 typedef double exaScalar;
 #define exa_gs_scalar gs_double
 #define exaScalarFormat "%lf"
+#define exaScalarMAX DBL_MAX
+#define exaScalarMIN DBL_MIN
 
 #define EXA_SCALAR MPI_DOUBLE
 
