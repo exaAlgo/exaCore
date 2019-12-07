fa9e0504e06b287a8526934830dec41a2a4c013f
Thilina Rathnayake <thilinarmtb@gmail.com>
2019-12-07 04:50:35 -0600

    [tests] Update tests

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
-tests=`ls t[0-9][0-9]-*`
+
+# Setup variables
+tests=(`ls -I '*.okl' -I '*.sh'`)
+backends=("/occa/cpu" "/occa/gpu/opencl")
+
+# Set debug to 0 to avoid output
+export EXA_DEBUG=0
+
+for t in "${tests[@]}"; do
+  for b in "${backends[@]}"; do
+    ./${t} ${b} >out 2>err
+    wait $!
+    if [ ! -s out ] && [ ! -s err ]; then
+      echo "Test: ${t} ${b} ok."
+    else
+      echo "Test: ${t} ${b} not ok."
+    fi
+  done
+done
+
+# Clean up
+rm err out
